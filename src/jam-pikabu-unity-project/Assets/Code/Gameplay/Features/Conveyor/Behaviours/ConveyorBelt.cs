edf8d2196b6885986cc2c601530974b3d6696640
OutdoorHawk <dz110699@gmail.com>
2024-12-07 00:55:22 +0400

    conveyr

---
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/Conveyor/Behaviours/ConveyorBelt.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Code.Gameplay.Features.Conveyor.Behaviours
+{
+    public class ConveyorBelt : MonoBehaviour
+    {
+        [SerializeField] private Vector2 conveyorForce = new(1f, 0f);
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            Rigidbody2D rb = other.attachedRigidbody;
+            if (rb != null)
+            {
+                rb.AddForce(conveyorForce, ForceMode2D.Force);
+            }
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            Rigidbody2D rb = other.rigidbody;
+            if (rb != null)
+            {
+                rb.linearVelocity = conveyorForce;
+            }
+        }
+    }
+}
