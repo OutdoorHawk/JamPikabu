b598f60c5f737241e4a14bfa067d1eedf155982f
OutdoorHawk <dz110699@gmail.com>
2024-12-07 13:19:43 +0400

    layout wip

---
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/Customers/Config/CustomerSetup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Code.Gameplay.StaticData;
+using UnityEngine;
+
+namespace Code.Gameplay.Features.Customers.Config
+{
+    [Serializable]
+    public class CustomerSetup : BaseData
+    {
+        public Sprite Sprite;
+    }
+}
---
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/Customers/Config/CustomerStaticData.cs
@@ -0,0 +1,11 @@
+﻿using Code.Gameplay.StaticData;
+using UnityEngine;
+
+namespace Code.Gameplay.Features.Customers.Config
+{
+    [CreateAssetMenu(menuName = "StaticData/" + nameof(CustomerStaticData), fileName = "Customer")]
+    public class CustomerStaticData : BaseStaticData<CustomerSetup>
+    {
+        
+    }
+}
