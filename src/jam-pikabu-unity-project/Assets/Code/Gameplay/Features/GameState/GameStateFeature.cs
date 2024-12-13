73f348e20dc48a1192a51561f5da2f0cbf53581f
OutdoorHawk <dz110699@gmail.com>
2024-12-14 01:17:42 +0400

    removed unused states

--- src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/GameState/GameStateFeature.cs
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/GameState/GameStateFeature.cs
@@ -8,18 +8,18 @@ namespace Code.Gameplay.Features.GameState
         public GameStateFeature(ISystemFactory systems)
         {
             Add(systems.Create<InitializeGameStateSystem>());
+
+            Add(systems.Create<EnterRoundPreparationGameStateSystem>());
+            Add(systems.Create<EnterRoundLoopGameState>());
+            Add(systems.Create<EnterRoundCompleteGameStateSystem>());
+            Add(systems.Create<EnterEndDayGameStateSystem>());
+            Add(systems.Create<EnterBeginDayGameStateSystem>());
             
             Add(systems.Create<ProcessBeginDayStateSystem>());
             Add(systems.Create<ProcessRoundPreparationStateSystem>());
             Add(systems.Create<ProcessRoundLoopStateSystem>());
             Add(systems.Create<ProcessRoundCompletionStateSystem>());
             Add(systems.Create<ProcessEndDayStateSystem>());
-            
-            Add(systems.Create<EnterRoundPreparationGameStateSystem>());
-            Add(systems.Create<EnterRoundLoopGameState>());
-            Add(systems.Create<EnterRoundCompleteGameStateSystem>());
-            Add(systems.Create<EnterEndDayGameStateSystem>());
-            Add(systems.Create<EnterBeginDayGameStateSystem>());
         }
     }
 }
