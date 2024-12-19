0675963e8b7034bf8109e80d37b67d7b500b4d75
OutdoorHawk <dz110699@gmail.com>
2024-12-20 01:30:44 +0400

    hook collider setup

--- src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/Loot/Behaviours/LootItem.cs
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/Loot/Behaviours/LootItem.cs
@@ -10,9 +10,9 @@ namespace Code.Gameplay.Features.Loot.Behaviours
     public class LootItem : EntityDependant
     {
         [SerializeField] private SpriteRenderer _sprite;
+        [SerializeField] private SpriteRenderer _colliderRenderer;
 
         private IStaticDataService _staticData;
-        private CapsuleCollider2D _capsuleCollider2D;
         private PolygonCollider2D _polygonCollider2D;
         private ICollisionRegistry _collisionRegistry;
 
@@ -34,12 +34,14 @@ namespace Code.Gameplay.Features.Loot.Behaviours
         {
             LootSetup lootSetup = _staticData.GetStaticData<LootSettingsStaticData>().GetConfig(Entity.LootTypeId);
             _sprite.sprite = lootSetup.Icon;
+            _colliderRenderer.sprite = lootSetup.Icon;
             transform.localScale = Vector3.one * lootSetup.Size;
-            _polygonCollider2D = _sprite.gameObject.AddComponent<PolygonCollider2D>();
+            _polygonCollider2D = _colliderRenderer.gameObject.AddComponent<PolygonCollider2D>();
             _polygonCollider2D.useDelaunayMesh = true;
             //_capsuleCollider2D.size = Vector3.one * lootSetup.ColliderSize;
             _polygonCollider2D.sharedMaterial = _polygonCollider2D.attachedRigidbody.sharedMaterial;
             _collisionRegistry.Register(_polygonCollider2D.GetInstanceID(), Entity);
+            Destroy(_colliderRenderer);
         }
     }
 }
