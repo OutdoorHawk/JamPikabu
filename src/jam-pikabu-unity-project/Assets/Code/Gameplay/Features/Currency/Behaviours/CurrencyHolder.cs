38c55af75c19e838032c382688b04de1c4a7ac25
OutdoorHawk <dz110699@gmail.com>
2024-12-21 14:13:02 +0400

    hud fixes

--- src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/Currency/Behaviours/CurrencyHolder.cs
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/Currency/Behaviours/CurrencyHolder.cs
@@ -1,4 +1,5 @@
 ﻿using Code.Gameplay.Features.Currency.Service;
+using Code.Meta.Features.Days.Service;
 using Code.Meta.UI.Common;
 using UnityEngine;
 using Zenject;
@@ -10,12 +11,15 @@ namespace Code.Gameplay.Features.Currency.Behaviours
         public PriceInfo PlayerCurrentGold;
         public PriceInfo PlayerPluses;
         public PriceInfo PlayerMinuses;
+        public PriceInfo PlayerStars;
 
         private IGameplayCurrencyService _gameplayCurrencyService;
+        private IDaysService _daysService;
 
         [Inject]
-        private void Construct(IGameplayCurrencyService gameplayCurrencyService)
+        private void Construct(IGameplayCurrencyService gameplayCurrencyService, IDaysService daysService)
         {
+            _daysService = daysService;
             _gameplayCurrencyService = gameplayCurrencyService;
         }
 
@@ -41,6 +45,9 @@ namespace Code.Gameplay.Features.Currency.Behaviours
             PlayerCurrentGold.SetupPrice(_gameplayCurrencyService.GetCurrencyOfType(CurrencyTypeId.Gold), CurrencyTypeId.Gold);
             PlayerPluses.SetupPrice(_gameplayCurrencyService.GetCurrencyOfType(CurrencyTypeId.Plus), CurrencyTypeId.Plus);
             PlayerMinuses.SetupPrice(_gameplayCurrencyService.GetCurrencyOfType(CurrencyTypeId.Minus), CurrencyTypeId.Minus);
+
+            int allEarnedStars = _daysService.GetAllEarnedStars();
+            PlayerStars.SetupPrice(allEarnedStars, CurrencyTypeId.Star);
         }
 
         private void Refresh()
