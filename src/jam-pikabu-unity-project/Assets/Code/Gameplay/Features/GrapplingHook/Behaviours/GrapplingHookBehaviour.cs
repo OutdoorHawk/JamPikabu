67684e73ec15dffaebb8528c26e175f1dbede30c
OutdoorHawk <dz110699@gmail.com>
2025-01-07 22:06:31 +0400

    added hook colors

--- src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/GrapplingHook/Behaviours/GrapplingHookBehaviour.cs
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Features/GrapplingHook/Behaviours/GrapplingHookBehaviour.cs
@@ -4,10 +4,12 @@ using Code.Gameplay.Sound;
 using Code.Gameplay.Sound.Service;
 using Code.Infrastructure.View;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 using Zenject;
 using static System.Threading.CancellationTokenSource;
 using static Code.Common.Extensions.AsyncGameplayExtensions;
+using Random = UnityEngine.Random;
 
 namespace Code.Gameplay.Features.GrapplingHook.Behaviours
 {
@@ -18,16 +20,33 @@ namespace Code.Gameplay.Features.GrapplingHook.Behaviours
         [SerializeField] private float _ascentDelay = 0.25f;
         [SerializeField] private float _openClawsDelay = 0.2f;
         [SerializeField] private float _collectAnimationDelay = 1f;
+        [SerializeField] private Color _slowerColor;
+        [SerializeField] private Color _fasterColor;
 
         private ISoundService _soundService;
         private CancellationTokenSource _movementBlendSource;
         private CancellationTokenSource _speedModificationSource;
+
         private float _lastMoveDirection;
+
+        private Tween[] _colorTweens;
+        private Material[] _materials;
 
         [Inject]
         private void Construct(ISoundService soundService)
         {
             _soundService = soundService;
+        }
+
+        private void Start()
+        {
+            Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+            _materials = new Material[renderers.Length];
+            _colorTweens = new Tween[renderers.Length];
+
+            for (int i = 0; i < renderers.Length; i++)
+                _materials[i] = renderers[i].material;
         }
 
         public void SetupXMovement(float moveDirection)
@@ -94,17 +113,39 @@ namespace Code.Gameplay.Features.GrapplingHook.Behaviours
         {
             _speedModificationSource?.Cancel();
             _speedModificationSource = CreateLinkedTokenSource(destroyCancellationToken);
-            
+
             bool flip = Random.value < 0.5f;
 
             if (flip)
+            {
+                ApplyColor(_slowerColor, 0.25f);
                 factor *= -1;
+            }
+            else
+            {
+                ApplyColor(_fasterColor, 0.25f);
+            }
 
             Entity.ReplaceHookSpeedModifier(1 + factor);
-            
+
             await DelaySeconds(duration, _speedModificationSource.Token);
-            
+
             Entity.ReplaceHookSpeedModifier(1);
+            ApplyColor(Color.white, 0.25f);
+        }
+
+        private void ApplyColor(Color newColor, float duration)
+        {
+            for (int i = 0; i < _materials.Length; i++)
+            {
+                Material material = _materials[i];
+
+                _colorTweens[i]?.Kill();
+                _colorTweens[i] = material
+                        .DOColor(newColor, duration)
+                        .SetLink(gameObject)
+                    ;
+            }
         }
     }
 }
