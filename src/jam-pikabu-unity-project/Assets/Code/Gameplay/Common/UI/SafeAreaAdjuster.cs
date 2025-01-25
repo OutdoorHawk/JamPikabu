43367b0060179e7cc924f47620decb4e010342e4
OutdoorHawk <dz110699@gmail.com>
2025-01-25 17:30:31 +0400

    safe area fix

---
+++ src/jam-pikabu-unity-project/Assets/Code/Gameplay/Common/UI/SafeAreaAdjuster.cs
@@ -0,0 +1,35 @@
+﻿using Code.Gameplay.Windows.Factory;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Gameplay.Common.UI
+{
+    public class SafeAreaHorizontalAdjuster : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _targetRectTransform;
+        
+        private IUIFactory _uiFactory;
+
+        [Inject]
+        private void Construct
+        (
+            IUIFactory uiFactory
+        )
+        {
+            _uiFactory = uiFactory;
+        }
+
+        private void Start()
+        {
+            ApplySafeAreaAdjustment();
+        }
+
+        private void ApplySafeAreaAdjustment()
+        {
+            if (_targetRectTransform == null)
+                return;
+
+            _targetRectTransform.sizeDelta = Screen.safeArea.size / _uiFactory.Canvas.scaleFactor;
+        }
+    }
+}
